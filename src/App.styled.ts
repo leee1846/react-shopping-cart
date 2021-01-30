3c4668c64a600b9f2fb40169cd2645a0d7d24af0
leee1846 <leee1846@gmail.com>
2021-01-30 15:02:29 +0900

    style: item 컴포넌트 스타일링

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,5 +1,13 @@
 import Styled from "styled-components";
+import IconButton from "@material-ui/core/IconButton";
 
 export const Wrapper = Styled.div`
-  
+  margin: 40px;
+`;
+
+export const StyledButton = Styled(IconButton)`
+  position:fixed;
+  z-index:100;
+  right:20px;
+  top:20px;
 `;
--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -2,7 +2,7 @@ import Button from "@material-ui/core/Button";
 //types
 import { CartItemType } from "../App";
 //styles
-import { Wrapper } from "./../App.styled";
+import { Wrapper } from "./Item.style";
 
 type Props = {
   item: CartItemType;

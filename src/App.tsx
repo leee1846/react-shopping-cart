07df508dec79c63fe80a3efa6fbbd8b8c67f9afb
leee1846 <leee1846@gmail.com>
2021-01-30 14:12:09 +0900

    init: cra설치 및 npm 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+//components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+//styles
+import { Wrapper } from "./App.styled";
 
-function App() {
+const getProducts = async () =>
+  await (await fetch(`https://fakestoreapi.com/products`)).json();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ss</h1>
     </div>
   );
-}
+};
 
 export default App;

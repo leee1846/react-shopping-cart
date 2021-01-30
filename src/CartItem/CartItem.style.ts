549a0a1c2dcf267e02ff09ce029dfe05968e0034
leee1846 <leee1846@gmail.com>
2021-01-30 16:35:16 +0900

    style: itemCart 스타일링

--- src/CartItem/CartItem.style.ts
+++ src/CartItem/CartItem.style.ts
@@ -1,5 +1,24 @@
 import Styled from "styled-components";
 
 export const Wrapper = Styled.div`
-  
+  display:flex;
+  justify-content:space-between;
+  font-family:Arial, Helvetica, san-serif;
+  border-bottom: 1px solid lightblue;
+  padding-bottom:20px;
+
+  div {
+    flex:1;
+  }
+
+  .information, .buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  img {
+    max-width: 80px;
+    object-fit: cover;
+    margin-left: 40px;
+  }
 `;

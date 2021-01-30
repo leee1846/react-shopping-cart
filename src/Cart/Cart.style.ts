33a7a1f2879747134e087eca27d3c71a510f93f2
leee1846 <leee1846@gmail.com>
2021-01-30 15:18:04 +0900

    feature: 컴포넌트 cart 구현

---
+++ src/Cart/Cart.style.ts
@@ -0,0 +1,7 @@
+import Styled from "styled-components";
+
+export const Wrapper = Styled.aside`
+  font-family:Arial, Helvetica, san-serif;
+  width: 500px;
+  padding: 20px;
+`;
---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+//types
+import { CartItemType } from "../App";
+//style
+import { Wrapper } from "./CartItem.style";
+
+const CartItem: React.FC = () => {
+  return <div>Cart Item</div>;
+};
+
+export default CartItem;

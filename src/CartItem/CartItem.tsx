2cf3ac8b28fc4f29a149a833c6e8ed064ae464e2
leee1846 <leee1846@gmail.com>
2021-01-30 16:25:05 +0900

    feature: cartItem 태그구성

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -4,9 +4,46 @@ import Button from "@material-ui/core/Button";
 import { CartItemType } from "../App";
 //style
 import { Wrapper } from "./CartItem.style";
+import Item from "../Item/Item";
 
-const CartItem: React.FC = () => {
-  return <div>Cart Item</div>;
+type Props = {
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => {
+  return (
+    <Wrapper>
+      <div>
+        <h3>{item.title}</h3>
+        <div className='information'>
+          <p>Price: ${item.price}</p>
+          <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+        </div>
+        <div className='buttons'>
+          <Button
+            size='small'
+            disableElevation
+            variant='contained'
+            onClick={() => removeFromCart(item.id)}
+          >
+            -
+          </Button>
+          <p>{item.amount}</p>
+          <Button
+            size='small'
+            disableElevation
+            variant='contained'
+            onClick={() => addToCart(item)}
+          >
+            +
+          </Button>
+        </div>
+      </div>
+      <img src={item.image} alt={item.title}></img>
+    </Wrapper>
+  );
 };
 
 export default CartItem;
